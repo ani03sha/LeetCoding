981a789b43af22143cbd0aefac4c8d7d18f1af24
ani03sha <anirudh03sharma@gmail.com>
2025-01-12 12:05:18 +0530

    Add LeetCode #0094 - Binary Tree Inorder Traversal

---
+++ src/main/java/org/redquark/leetcoding/utils/TreeNode.java
@@ -0,0 +1,12 @@
+package org.redquark.leetcoding.utils;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
