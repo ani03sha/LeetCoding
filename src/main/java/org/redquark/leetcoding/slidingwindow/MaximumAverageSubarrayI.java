8d162912240e756d8d0adc0f8237e9814a863d4b
ani03sha <anirudh03sharma@gmail.com>
2025-01-21 04:35:42 +0530

    Add LeetCode #0643 - Maximum Average Subarray I

---
+++ src/main/java/org/redquark/leetcoding/slidingwindow/MaximumAverageSubarrayI.java
@@ -0,0 +1,43 @@
+package org.redquark.leetcoding.slidingwindow;
+
+public class MaximumAverageSubarrayI {
+
+    public double findMaxAverage(int[] nums, int k) {
+        // Special case
+        if (nums == null || nums.length < k) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        // Sum of k elements
+        int sum = 0;
+        // Max sum of k elements
+        int maxSum = 0;
+        // Calculate the sum of first k elements
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        maxSum = sum;
+        // Process remaining k elements
+        for (int i = k; i < nums.length; i++) {
+            // Remove the element from the left from the sum
+            sum -= nums[i - k];
+            // Add the current element to the sum
+            sum += nums[i];
+            // Update max sum, if required
+            maxSum = Math.max(maxSum, sum);
+        }
+        // Calculate and return the average
+        return maxSum * 1.0 / k;
+    }
+
+    public static void main(String[] args) {
+        final MaximumAverageSubarrayI maximumAverageSubarrayI = new MaximumAverageSubarrayI();
+
+        int[] nums = new int[]{1, 12, -5, -6, 50, 3};
+        int k = 4;
+        System.out.println(maximumAverageSubarrayI.findMaxAverage(nums, k));
+
+        nums = new int[]{5};
+        k = 1;
+        System.out.println(maximumAverageSubarrayI.findMaxAverage(nums, k));
+    }
+}
