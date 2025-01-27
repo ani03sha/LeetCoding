3e071f0d1f0e25127513ed526523ffd1048f19fc
ani03sha <anirudh03sharma@gmail.com>
2025-01-27 10:09:39 +0530

    Update LeetCode #0033 - Search In Rotated Sorted Array

--- src/main/java/org/redquark/leetcoding/binarysearch/SearchInRotatedSortedArray.java
+++ src/main/java/org/redquark/leetcoding/binarysearch/SearchInRotatedSortedArray.java
@@ -7,23 +7,29 @@ public class SearchInRotatedSortedArray {
         if (nums == null || nums.length == 0) {
             return -1;
         }
-        // Left and right pointers
+        // Left and right pointers for binary search
         int left = 0;
         int right = nums.length - 1;
-        // Process array from both ends
+        // Perform binary search within the range
         while (left <= right) {
             final int middle = left + (right - left) / 2;
+            // If we have found the target
             if (nums[middle] == target) {
                 return middle;
             }
-            if (nums[left] <= nums[middle]) {
-                if (target >= nums[left] && target <= nums[middle]) {
+            // If the left half is properly sorted
+            else if (nums[left] <= nums[middle]) {
+                // See if the target is between left and middle
+                if (nums[left] <= target && target < nums[middle]) {
                     right = middle - 1;
                 } else {
                     left = middle + 1;
                 }
-            } else {
-                if (nums[middle] <= target && target <= nums[right]) {
+            }
+            // If the left half is not properly sorted
+            else {
+                // See if the target is between middle and right
+                if (nums[middle] < target && target <= nums[right]) {
                     left = middle + 1;
                 } else {
                     right = middle - 1;
