a351e9be409c5ef09b7121293f0c967398e7047d
ani03sha <anirudh03sharma@gmail.com>
2025-01-27 09:44:22 +0530

    Update LeetCode #0050 - PowXn

--- src/main/java/org/redquark/leetcoding/math/PowXn.java
+++ src/main/java/org/redquark/leetcoding/math/PowXn.java
@@ -7,13 +7,17 @@ public class PowXn {
         if (n == 0) {
             return 1;
         }
-        // Divide the power by half and recurse
-        double temp = myPow(x, n / 2);
-        // If the power is a multiple of 2
-        if (n % 2 == 0) {
-            return temp * temp;
+        // One special case
+        if (n == Integer.MIN_VALUE) {
+            x *= x;
+            n /= 2;
         }
-        return n > 0 ? (x * temp * temp) : (temp * temp / x);
+        // If the exponent is negative
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
     }
 
     public static void main(String[] args) {
