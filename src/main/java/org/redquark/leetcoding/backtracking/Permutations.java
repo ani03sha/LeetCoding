a33dfff9e0f4619e0ad3fefd257d45e581baeea3
ani03sha <anirudh03sharma@gmail.com>
2025-01-27 10:28:14 +0530

    Update LeetCode #0046 - Permutations

--- src/main/java/org/redquark/leetcoding/backtracking/Permutations.java
+++ src/main/java/org/redquark/leetcoding/backtracking/Permutations.java
@@ -32,16 +32,54 @@ public class Permutations {
         }
     }
 
+    public List<List<Integer>> permuteOptimized(int[] nums) {
+        // List to store final output
+        final List<List<Integer>> permutations = new ArrayList<>();
+        // Special case
+        if (nums == null || nums.length == 0) {
+            return permutations;
+        }
+        // Perform backtracking
+        backtrack(nums, 0, permutations);
+        return permutations;
+    }
+
+    private void backtrack(int[] nums, int index, List<List<Integer>> permutations) {
+        // Base case
+        if (index == nums.length) {
+            final List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            permutations.add(new ArrayList<>(permutation));
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, i, index);
+            backtrack(nums, index + 1, permutations);
+            swap(nums, i, index);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        final int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public static void main(String[] args) {
         final Permutations permutations = new Permutations();
 
         int[] nums = new int[]{1, 2, 3};
         System.out.println(permutations.permute(nums));
+        System.out.println(permutations.permuteOptimized(nums));
 
         nums = new int[]{0, 1};
         System.out.println(permutations.permute(nums));
+        System.out.println(permutations.permuteOptimized(nums));
 
         nums = new int[]{1};
         System.out.println(permutations.permute(nums));
+        System.out.println(permutations.permuteOptimized(nums));
     }
 }
