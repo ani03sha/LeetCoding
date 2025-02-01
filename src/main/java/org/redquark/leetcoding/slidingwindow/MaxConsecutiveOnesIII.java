05414b904f81593797e53ca4e788a72255f0ba3b
ani03sha <anirudh03sharma@gmail.com>
2025-02-01 10:50:32 +0530

    Add LeetCode #1004 - Max Consecutive Ones III

---
+++ src/main/java/org/redquark/leetcoding/slidingwindow/MaxConsecutiveOnesIII.java
@@ -0,0 +1,63 @@
+package org.redquark.leetcoding.slidingwindow;
+
+public class MaxConsecutiveOnesIII {
+
+    public int longestOnes(int[] nums, int k) {
+        // Special case
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        final int n = nums.length;
+        // One more special case
+        if (n < k) {
+            return n;
+        }
+        // Left and right pointers of the sliding window
+        int left = 0;
+        int right = 0;
+        // Longest length
+        int longestLength = 0;
+        // Current length of valid window
+        int currentLength = 0;
+        // Process the binary array
+        while (right < n) {
+            // If the current element is one
+            if (nums[right] == 1) {
+                currentLength++;
+                right++;
+            }
+            // If the current element is zero, but we have margin
+            // to flip it
+            else if (nums[right] == 0 && k > 0) {
+                currentLength++;
+                k--;
+                right++;
+            }
+            // If we encounter zero, but we don't have margin to flip it
+            else {
+                // Update the longestLength
+                longestLength = Math.max(longestLength, right - left);
+                // Move left pointer to the first zero
+                while (nums[left] == 1) {
+                    left++;
+                }
+                left++;
+                right++;
+                currentLength = right - left;
+            }
+        }
+        return Math.max(currentLength, longestLength);
+    }
+
+    public static void main(String[] args) {
+        final MaxConsecutiveOnesIII maxConsecutiveOnesIII = new MaxConsecutiveOnesIII();
+
+        int[] nums = new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+        int k = 2;
+        System.out.println(maxConsecutiveOnesIII.longestOnes(nums, k));
+
+        nums = new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+        k = 3;
+        System.out.println(maxConsecutiveOnesIII.longestOnes(nums, k));
+    }
+}
