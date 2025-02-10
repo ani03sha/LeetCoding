6424c603aad57df3a6d913484d657459768e58e8
ani03sha <anirudh03sharma@gmail.com>
2025-02-10 05:52:14 +0530

    Add LeetCode #0152 - Maximum Product Subarray

--- src/main/java/org/redquark/leetcoding/dynamicprogramming/MaximumProductSubarray.java
+++ src/main/java/org/redquark/leetcoding/dynamicprogramming/MaximumProductSubarray.java
@@ -5,24 +5,26 @@ public class MaximumProductSubarray {
     public int maxProduct(int[] nums) {
         // Special case
         if (nums == null || nums.length == 0) {
-            throw new IllegalArgumentException("Invalid inputs!");
+            return 0;
         }
-        // Variable to keep track of maximum product overall
-        int globalMax = nums[0];
-        // Variable to keep track of max product until index i
-        int localMax = nums[0];
-        // Variable to keep track of min product until index i
-        int localMin = nums[0];
-        // Process the remaining elements in the array
+        // Maximum subarray product when the entire array is under consideration
+        int globalMaxProduct = nums[0];
+        // Maximum subarray product when a subarray is under consideration
+        int localMaxProduct = nums[0];
+        // Minimum subarray product when a subarray is under consideration
+        int localMinProduct = nums[0];
+        // Process all elements in the array
         for (int i = 1; i < nums.length; i++) {
-            // Store localMax for future calculations
-            final int temp = localMax;
-            // Update localMax, localMin and globalMax
-            localMax = Math.max(nums[i], Math.max(nums[i] * temp, nums[i] * localMin));
-            localMin = Math.min(nums[i], Math.min(nums[i] * temp, nums[i] * localMin));
-            globalMax = Math.max(localMax, globalMax);
+            // Store localMaxProduct in a temp variable for later use
+            final int temp = localMaxProduct;
+            // Update localMaxProduct after considering current element
+            localMaxProduct = Math.max(nums[i], Math.max(nums[i] * temp, nums[i] * localMinProduct));
+            // Update localMinProduct after considering the current element
+            localMinProduct = Math.min(nums[i], Math.min(nums[i] * temp, nums[i] * localMinProduct));
+            // Update globalMaxProduct after considering subarray ending at current index
+            globalMaxProduct = Math.max(localMaxProduct, globalMaxProduct);
         }
-        return globalMax;
+        return globalMaxProduct;
     }
 
     public static void main(String[] args) {
