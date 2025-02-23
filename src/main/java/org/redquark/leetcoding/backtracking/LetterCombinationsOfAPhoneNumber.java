690fbed0ab3b58b16b04be6dcf27c31a1c700ef1
ani03sha <anirudh03sharma@gmail.com>
2025-02-24 04:59:04 +0530

    Add LeetCode #0017 - Letter Combinations Of A Phone Number

--- src/main/java/org/redquark/leetcoding/backtracking/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/org/redquark/leetcoding/backtracking/LetterCombinationsOfAPhoneNumber.java
@@ -43,11 +43,54 @@ public class LetterCombinationsOfAPhoneNumber {
         }
     }
 
+    public List<String> letterCombinationsOptimized(String digits) {
+        // List to store all the combinations
+        List<String> combinations = new ArrayList<>();
+        // Special case
+        if (digits == null || digits.isEmpty()) {
+            return combinations;
+        }
+        // Digits to characters mapping
+        final String[] mappings = {
+                "",
+                "",
+                "abc",
+                "def",
+                "ghi",
+                "jkl",
+                "mno",
+                "pqrs",
+                "tuv",
+                "wxyz"
+        };
+        // Add empty string to the list
+        combinations.add("");
+        // Process for each character in the 'digits' string
+        for (char c : digits.toCharArray()) {
+            // The alphabets corresponding to this digit
+            final String alphabets = mappings[c - '0'];
+            // List to store current combinations of characters
+            final List<String> currentCombinations = new ArrayList<>();
+            // For every existing combination in the combinations list,
+            // we make a pair with the current set of alphabets
+            for (String combination : combinations) {
+                for (char alphabet : alphabets.toCharArray()) {
+                    currentCombinations.add(combination + alphabet);
+                }
+            }
+            combinations = currentCombinations;
+        }
+        return combinations;
+    }
+
     public static void main(String[] args) {
         final LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumber();
 
         System.out.println(letterCombinationsOfAPhoneNumber.letterCombinations("23"));
+        System.out.println(letterCombinationsOfAPhoneNumber.letterCombinationsOptimized("23"));
         System.out.println(letterCombinationsOfAPhoneNumber.letterCombinations(""));
+        System.out.println(letterCombinationsOfAPhoneNumber.letterCombinationsOptimized(""));
         System.out.println(letterCombinationsOfAPhoneNumber.letterCombinations("2"));
+        System.out.println(letterCombinationsOfAPhoneNumber.letterCombinationsOptimized("2"));
     }
 }
