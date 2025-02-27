6280ad3ed15671d78e6277b2cc727f28ee89c87c
ani03sha <anirudh03sharma@gmail.com>
2025-02-27 20:13:44 +0530

    Add LeetCode #0688 - Knight Probability In Chessboard

---
+++ src/main/java/org/redquark/leetcoding/dynamicprogramming/KnightProbabilityInChessboard.java
@@ -0,0 +1,34 @@
+package org.redquark.leetcoding.dynamicprogramming;
+
+public class KnightProbabilityInChessboard {
+
+    public double knightProbability(int n, int k, int row, int column) {
+        // Eight directions
+        final int[][] directions = new int[][]{{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}};
+        // Lookup table to store probability of being inside table after k moves
+        final double[][][] lookup = new double[n][n][k + 1];
+        // Recursively find the solution
+        return dfs(n, k, row, column, directions, lookup);
+    }
+
+    private double dfs(int n, int k, int row, int column, int[][] directions, double[][][] lookup) {
+        // Check for valid cells
+        if (row < 0 || row >= n || column < 0 || column >= n) {
+            return 0;
+        }
+        // If we have moved k times
+        if (k == 0) {
+            return 1;
+        }
+        // Check if we have already tried this cell
+        if (lookup[row][column][k] != 0) {
+            return lookup[row][column][k];
+        }
+        // Probability for this move
+        double probability = 0;
+        for (int[] direction : directions) {
+            probability += 0.125 * dfs(n, k - 1, row + direction[0], column + direction[1], directions, lookup);
+        }
+        return lookup[row][column][k] = probability;
+    }
+}
