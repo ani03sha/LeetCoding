c456e88c50d3e1aabe973150e9d775da2c1e5541
ani03sha <anirudh03sharma@gmail.com>
2025-04-19 04:54:10 +0530

    Add LeetCode #0480 - Sliding Window Median

--- src/main/java/org/redquark/leetcoding/slidingwindow/SlidingWindowMedian.java
+++ src/main/java/org/redquark/leetcoding/slidingwindow/SlidingWindowMedian.java
@@ -8,25 +8,26 @@ public class SlidingWindowMedian {
 
     public double[] medianSlidingWindow(int[] nums, int k) {
         final int n = nums.length;
-        // Array to store the medians
+        // Array to store the medians of the sliding windows
         final double[] medians = new double[n - k + 1];
+        // Comparator to compare the elements
         final Comparator<Integer> comparator = (a, b) -> {
             if (nums[a] != nums[b]) {
                 return Integer.compare(nums[a], nums[b]);
-            } else {
-                return a - b;
             }
+            return a - b;
         };
-        // We will create two Tree Sets - one for storing values that are less than median
-        // and another for storing values that are greater than or equal to median
+        // TreeSet to contain elements smaller than median
         final TreeSet<Integer> left = new TreeSet<>(comparator.reversed());
+        // TreeSet to contain elements greater than median
         final TreeSet<Integer> right = new TreeSet<>(comparator);
-        // Add first k elements to the left set
+        // Add first k elements to the left
         for (int i = 0; i < k; i++) {
             left.add(i);
         }
-        // Balance left and right sets
+        // Balance both tree sets
         balance(left, right);
+        // Calculate the median of the first window
         medians[0] = calculateMedian(nums, left, right, k);
         // Process remaining elements
         for (int i = k; i < n; i++) {
@@ -41,17 +42,17 @@ public class SlidingWindowMedian {
         return medians;
     }
 
-    private void balance(TreeSet<Integer> left, TreeSet<Integer> right) {
-        while (left.size() > right.size()) {
-            right.add(left.removeFirst());
+    private double calculateMedian(int[] nums, TreeSet<Integer> left, TreeSet<Integer> right, int k) {
+        if (k % 2 == 0) {
+            return (nums[left.first()] * 1.0 + nums[right.first()]) / 2;
+        } else {
+            return nums[right.first()];
         }
     }
 
-    private double calculateMedian(int[] nums, TreeSet<Integer> left, TreeSet<Integer> right, int k) {
-        if (k % 2 == 0) {
-            return ((double) nums[left.first()] + (double) nums[right.first()]) / 2;
-        } else {
-            return nums[right.first()];
+    private void balance(TreeSet<Integer> left, TreeSet<Integer> right) {
+        while (left.size() > right.size()) {
+            right.add(left.removeFirst());
         }
     }
 
