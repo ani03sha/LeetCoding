38353ca92da14f04788ca74305011771a428bf55
ani03sha <anirudh03sharma@gmail.com>
2025-04-22 19:26:14 +0530

    Add LeetCode #0045 - Jump Game II

---
+++ src/main/java/org/redquark/leetcoding/greedy/JumpGameII.java
@@ -0,0 +1,35 @@
+package org.redquark.leetcoding.greedy;
+
+public class JumpGameII {
+
+    public int jump(int[] nums) {
+        final int n = nums.length;
+        // Jumps required
+        int jumps = 0;
+        // Current position
+        int currentPosition = 0;
+        // Destination reached from the current index
+        int destination = 0;
+        // Process elements in the array
+        for (int i = 0; i < nums.length - 1; i++) {
+            // Maximum distance reached from the current position
+            destination = Math.max(destination, i + nums[i]);
+            // If we need to take jump
+            if (currentPosition == i) {
+                currentPosition = destination;
+                jumps++;
+            }
+        }
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        final JumpGameII jumpGameII = new JumpGameII();
+
+        int[] nums = new int[]{2, 3, 1, 1, 4};
+        System.out.println(jumpGameII.jump(nums));
+
+        nums = new int[]{2, 3, 0, 1, 4};
+        System.out.println(jumpGameII.jump(nums));
+    }
+}
