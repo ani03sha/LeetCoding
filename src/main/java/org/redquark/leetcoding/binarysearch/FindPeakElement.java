c503a4897e480f16f01dd34a9a3c4a630e47bbd7
ani03sha <anirudh03sharma@gmail.com>
2025-04-27 22:12:16 +0530

    Update LeetCode #0162 - Find Peak Element

--- src/main/java/org/redquark/leetcoding/binarysearch/FindPeakElement.java
+++ src/main/java/org/redquark/leetcoding/binarysearch/FindPeakElement.java
@@ -3,22 +3,23 @@ package org.redquark.leetcoding.binarysearch;
 public class FindPeakElement {
 
     public int findPeakElement(int[] nums) {
-        // Left and right pointers for the binary search
+        // Left and right pointers for binary search
         int left = 0;
         int right = nums.length - 1;
         // Perform binary search
-        while (left < right) {
+        while (left <= right) {
             final int middle = left + (right - left) / 2;
-            // If the peak lies to the right of the middle element
-            if (nums[middle] < nums[middle + 1]) {
+            // If middle is either at the boundary of array or it is greater
+            // than its neighboring elements, we return it
+            if ((middle == nums.length - 1 || nums[middle + 1] < nums[middle]) && (middle == 0 || nums[middle - 1] < nums[middle])) {
+                return middle;
+            } else if (nums[middle + 1] > nums[middle]) {
                 left = middle + 1;
-            }
-            // If the peak exists to the left of the middle
-            else {
-                right = middle;
+            } else {
+                right = middle - 1;
             }
         }
-        return left;
+        return -1;
     }
 
     public static void main(String[] args) {
