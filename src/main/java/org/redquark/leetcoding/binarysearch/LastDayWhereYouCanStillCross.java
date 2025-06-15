a8f25ca7467a539111471003c5abccd9cafdc53c
ani03sha <anirudh03sharma@gmail.com>
2025-06-15 05:41:08 +0530

    Update LeetCode #1970 - Last Day Wher eYou Can Still Cross

--- src/main/java/org/redquark/leetcoding/binarysearch/LastDayWhereYouCanStillCross.java
+++ src/main/java/org/redquark/leetcoding/binarysearch/LastDayWhereYouCanStillCross.java
@@ -5,19 +5,26 @@ import java.util.Queue;
 
 public class LastDayWhereYouCanStillCross {
 
-    public int latestDayToCrossBFS(int row, int col, int[][] cells) {
+    public int latestDayToCrossBFS(int row, int column, int[][] cells) {
         // Last day when we can cross
         int lastDay = 0;
         // Four directions
-        final int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        // Left and right pointers for binary search
+        final int[][] directions = new int[][]{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+        // We need to perform binary search to identify the
+        // last day on which we can still cross with the
+        // following rules -
+        // 1. If we can cross on day i, we can cross any day before it
+        // 2. If you can't cross on day i, we can't cross on day after it
+        // Since every day one cell is flooded, so our search space is
+        // from day 1 to day row * column
         int left = 1;
-        int right = row * col;
-        // Perform binary search in this space
+        int right = row * column;
+        // Perform binary search on this space
         while (left <= right) {
+            // Let's try middle-th day
             final int middle = left + (right - left) / 2;
-            // Check if we can cross on "middle"-th day
-            if (canCrossBFS(row, col, cells, middle, directions)) {
+            // Perform BFS on this day
+            if (canCrossBFS(row, column, middle, cells, directions)) {
                 lastDay = middle;
                 left = middle + 1;
             } else {
@@ -27,55 +34,64 @@ public class LastDayWhereYouCanStillCross {
         return lastDay;
     }
 
-    private boolean canCrossBFS(int row, int col, int[][] cells, int day, int[][] directions) {
-        // Create a grid
-        final int[][] grid = new int[row][col];
-        // Queue for BFS
-        final Queue<int[]> queue = new LinkedList<>();
-        // Set all cells until day to 1
-        for (int i = 0; i < day; i++) {
+    private boolean canCrossBFS(int row, int column, int currentDay, int[][] cells, int[][] directions) {
+        // Create a grid of land
+        final int[][] grid = new int[row][column];
+        // Flood this grid until the currentDay
+        for (int i = 0; i < currentDay; i++) {
             grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
         }
-        // Add all cells in the first row to queue
-        for (int i = 0; i < col; i++) {
+        // Queue to perform BFS
+        final Queue<int[]> queue = new LinkedList<>();
+        // Add all land cells from the first row to the queue
+        for (int i = 0; i < column; i++) {
             if (grid[0][i] == 0) {
                 queue.offer(new int[]{0, i});
                 grid[0][i] = -1; // Mark as visited
             }
         }
-        // Perform BFS
+        // Perform BFS here
         while (!queue.isEmpty()) {
+            // Get current cell
             final int[] current = queue.remove();
             // If we have reached the last row
             if (current[0] == row - 1) {
                 return true;
             }
-            // Traverse in four directions
+            // Traverse in all four directions
             for (int[] direction : directions) {
                 final int x = current[0] + direction[0];
                 final int y = current[1] + direction[1];
-                // Check for cell validity
-                if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] != 0) {
+                // Check of cell validity
+                if (x < 0 || x >= row || y < 0 || y >= column || grid[x][y] != 0) {
                     continue;
                 }
-                grid[x][y] = -1;
                 queue.offer(new int[]{x, y});
+                // Mark the cell visited
+                grid[x][y] = -1;
             }
         }
         return false;
     }
 
-    public int latestDayToCrossDFS(int row, int col, int[][] cells) {
+    public int latestDayToCrossDFS(int row, int column, int[][] cells) {
         // Last day when we can cross
         int lastDay = 0;
-        // Left and right pointers for binary search
+        // We need to perform binary search to identify the
+        // last day on which we can still cross with the
+        // following rules -
+        // 1. If we can cross on day i, we can cross any day before it
+        // 2. If you can't cross on day i, we can't cross on day after it
+        // Since every day one cell is flooded, so our search space is
+        // from day 1 to day row * column
         int left = 1;
-        int right = row * col;
-        // Perform binary search in this space
+        int right = row * column;
+        // Perform binary search on this space
         while (left <= right) {
+            // Let's try middle-th day
             final int middle = left + (right - left) / 2;
-            // Check if we can cross on "middle"-th day
-            if (canCrossDFS(row, col, cells, middle)) {
+            // Perform BFS on this day
+            if (canCrossDFS(row, column, middle, cells)) {
                 lastDay = middle;
                 left = middle + 1;
             } else {
@@ -85,25 +101,25 @@ public class LastDayWhereYouCanStillCross {
         return lastDay;
     }
 
-    private boolean canCrossDFS(int row, int col, int[][] cells, int day) {
+    private boolean canCrossDFS(int row, int column, int day, int[][] cells) {
         // Create a grid
-        final int[][] grid = new int[row][col];
+        final int[][] grid = new int[row][column];
         // Set all cells until day to 1
         for (int i = 0; i < day; i++) {
             grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
         }
         // Check for all cells in the first row
-        for (int i = 0; i < col; i++) {
-            if (grid[0][i] == 0 && dfs(grid, 0, i, row, col)) {
+        for (int i = 0; i < column; i++) {
+            if (grid[0][i] == 0 && dfs(grid, 0, i, row, column)) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(int[][] grid, int i, int j, int row, int col) {
+    private boolean dfs(int[][] grid, int i, int j, int row, int column) {
         // Base case
-        if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != 0) {
+        if (i < 0 || i >= row || j < 0 || j >= column || grid[i][j] != 0) {
             return false;
         }
         // If we have reached the last row
@@ -113,34 +129,38 @@ public class LastDayWhereYouCanStillCross {
         // Mark visited
         grid[i][j] = -1;
         // Traverse in four directions
-        return dfs(grid, i - 1, j, row, col)
-                || dfs(grid, i + 1, j, row, col)
-                || dfs(grid, i, j - 1, row, col)
-                || dfs(grid, i, j + 1, row, col);
+        return dfs(grid, i - 1, j, row, column)
+                || dfs(grid, i + 1, j, row, column)
+                || dfs(grid, i, j - 1, row, column)
+                || dfs(grid, i, j + 1, row, column);
     }
 
-    public int latestDayToCrossUnionFind(int row, int col, int[][] cells) {
-        final UnionFind unionFind = new UnionFind(row * col + 2);
-        final int[][] grid = new int[row][col];
+    public int latestDayToCrossUnionFind(int row, int column, int[][] cells) {
+        final UnionFind unionFind = new UnionFind(row * column + 2);
+        // Create a new grid
+        final int[][] grid = new int[row][column];
+        // Four directions
         final int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        // Traverse for all given cells
+        // Traverse for all given cells in reverse order because
+        // the last day when there's a path is the first day in the
+        // reverse order
         for (int i = cells.length - 1; i >= 0; i--) {
             final int r = cells[i][0] - 1;
             final int c = cells[i][1] - 1;
-            // Mark the cell as blocked
+            // Mark the cell as blocked (flooded)
             grid[r][c] = 1;
             // Flattened index for the current cell
-            final int index1 = r * col + c + 1;
+            final int index1 = r * column + c + 1;
             // Check in all four directions
             for (int[] direction : directions) {
                 final int x = r + direction[0];
                 final int y = c + direction[1];
                 // Check for cell validity
-                if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] != 1) {
+                if (x < 0 || x >= row || y < 0 || y >= column || grid[x][y] != 1) {
                     continue;
                 }
                 // Flattened index for the adjacent cell
-                final int index2 = x * col + y + 1;
+                final int index2 = x * column + y + 1;
                 unionFind.union(index1, index2);
             }
             if (r == 0) {
@@ -149,10 +169,10 @@ public class LastDayWhereYouCanStillCross {
             }
             if (r == row - 1) {
                 // Connect the last row to the virtual bottom cell
-                unionFind.union(row * col + 1, index1);
+                unionFind.union(row * column + 1, index1);
             }
             // Check if the virtual top and bottom cells are connected
-            if (unionFind.find(0) == unionFind.find(row * col + 1)) {
+            if (unionFind.find(0) == unionFind.find(row * column + 1)) {
                 return i; // Return the last day when we can cross
             }
         }
@@ -160,23 +180,23 @@ public class LastDayWhereYouCanStillCross {
     }
 
     static class UnionFind {
-        private final int[] parent;
-        private final int[] rank;
+        private final int[] parents;
+        private final int[] ranks;
 
         public UnionFind(int size) {
-            this.parent = new int[size];
-            this.rank = new int[size];
+            this.parents = new int[size];
+            this.ranks = new int[size];
             for (int i = 0; i < size; i++) {
-                parent[i] = i;
-                rank[i] = 1;
+                parents[i] = i;
+                ranks[i] = 1;
             }
         }
 
         public int find(int param) {
-            if (parent[param] != param) {
-                parent[param] = find(parent[param]);
+            if (parents[param] != param) {
+                parents[param] = find(parents[param]);
             }
-            return parent[param];
+            return parents[param];
         }
 
         public void union(int x, int y) {
@@ -185,12 +205,12 @@ public class LastDayWhereYouCanStillCross {
             if (rootX == rootY) {
                 return;
             }
-            if (this.rank[rootX] <= this.rank[rootY]) {
-                this.parent[rootX] = rootY;
-                this.rank[rootY] += this.rank[rootX];
+            if (this.ranks[rootX] <= this.ranks[rootY]) {
+                this.parents[rootX] = rootY;
+                this.ranks[rootY] += this.ranks[rootX];
             } else {
-                this.parent[rootY] = rootX;
-                this.rank[rootX] += this.rank[rootY];
+                this.parents[rootY] = rootX;
+                this.ranks[rootX] += this.ranks[rootY];
             }
         }
     }
