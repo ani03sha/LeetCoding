cabee4653e14b2564443b4398bcec5342f1c206a
ani03sha <anirudh03sharma@gmail.com>
2025-06-15 07:30:23 +0530

    Update LeetCode #0079 - Word Search

--- src/main/java/org/redquark/leetcoding/backtracking/WordSearch.java
+++ src/main/java/org/redquark/leetcoding/backtracking/WordSearch.java
@@ -42,6 +42,49 @@ public class WordSearch {
         return visited[i][j] = false;
     }
 
+    public boolean existSpaceOptimized(char[][] board, String word) {
+        // Special case
+        if (board == null || board.length == 0 || word == null || word.isEmpty()) {
+            return false;
+        }
+        // Dimensions of the board
+        final int m = board.length;
+        final int n = board[0].length;
+        // Process every cell on the board
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Perform backtracking
+                if (backtrack(board, i, j, m, n, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean backtrack(char[][] board, int i, int j, int m, int n, String word, int index) {
+        // Base condition
+        if (index == word.length()) {
+            return true;
+        }
+        // Check for cell validity
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word.charAt(index) || board[i][j] == '#') {
+            return false;
+        }
+        // Current character
+        final char c = board[i][j];
+        // Mark current cell as visited
+        board[i][j] = '#';
+        // Perform DFS in all four directions
+        final boolean w = backtrack(board, i - 1, j, m, n, word, index + 1);
+        final boolean x = backtrack(board, i + 1, j, m, n, word, index + 1);
+        final boolean y = backtrack(board, i, j - 1, m, n, word, index + 1);
+        final boolean z = backtrack(board, i, j + 1, m, n, word, index + 1);
+        // Reset character
+        board[i][j] = c;
+        return w || x || y || z;
+    }
+
     public static void main(String[] args) {
         final WordSearch wordSearch = new WordSearch();
 
@@ -50,11 +93,14 @@ public class WordSearch {
         };
         String word = "ABCCED";
         System.out.println(wordSearch.exist(board, word));
+        System.out.println(wordSearch.existSpaceOptimized(board, word));
 
         word = "SEE";
         System.out.println(wordSearch.exist(board, word));
+        System.out.println(wordSearch.existSpaceOptimized(board, word));
 
         word = "ABCB";
         System.out.println(wordSearch.exist(board, word));
+        System.out.println(wordSearch.existSpaceOptimized(board, word));
     }
 }
