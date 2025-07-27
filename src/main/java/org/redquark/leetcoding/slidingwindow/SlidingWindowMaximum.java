0a8392e0d348d505e9f1722573aa0d3aa77d65a9
ani03sha <anirudh03sharma@gmail.com>
2025-07-27 11:21:01 +0530

    Update LeetCode #0239 - Sliding Window Maximum

--- src/main/java/org/redquark/leetcoding/slidingwindow/SlidingWindowMaximum.java
+++ src/main/java/org/redquark/leetcoding/slidingwindow/SlidingWindowMaximum.java
@@ -7,33 +7,34 @@ import java.util.Deque;
 public class SlidingWindowMaximum {
 
     public int[] maxSlidingWindow(int[] nums, int k) {
-        // Special case
+        // Edge case
         if (nums == null || nums.length < k) {
             return new int[]{};
         }
         final int n = nums.length;
-        // Array to store the maximum sliding window
-        final int[] slidingWindowMax = new int[n - k + 1];
-        // Deque to store indices of elements of sliding window
-        final Deque<Integer> deque = new ArrayDeque<>();
-        // Process elements in the array
+        // Array to store the sliding window
+        final int[] maxOfSlidingWindow = new int[n - k + 1];
+        // Monotonic deque for storing indices w.r.t a sliding window
+        final Deque<Integer> window = new ArrayDeque<>();
+        // Process all elements in the array
         for (int i = 0; i < n; i++) {
-            if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
-                deque.pollFirst();
+            // Remove elements from deque that are out of window bounds
+            if (!window.isEmpty() && window.peekFirst() < i - k + 1) {
+                window.removeFirst();
             }
-            // Remove elements from thr deque which are less than the
-            // current elements as they can never be max
-            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
-                deque.pollLast();
+            // Remove elements from the deque that are smaller than the
+            // current elements as they can never be the maximum for that window
+            while (!window.isEmpty() && nums[window.peekLast()] < nums[i]) {
+                window.removeLast();
             }
             // Add current index to the deque
-            deque.offer(i);
+            window.offer(i);
+            // When window size is reached
             if (i >= k - 1) {
-                // noinspection DataFlowIssue
-                slidingWindowMax[i - k + 1] = nums[deque.peekFirst()];
+                maxOfSlidingWindow[i - k + 1] = nums[window.peekFirst()];
             }
         }
-        return slidingWindowMax;
+        return maxOfSlidingWindow;
     }
 
     public static void main(String[] args) {
