d19d725d0b34ccd23f61e19e28e9431efab78c49
ani03sha <anirudh03sharma@gmail.com>
2025-09-04 07:48:25 +0530

    Update LeetCode #0127 - Word Ladder

--- src/main/java/org/redquark/leetcoding/bfs/WordLadder.java
+++ src/main/java/org/redquark/leetcoding/bfs/WordLadder.java
@@ -59,6 +59,60 @@ public class WordLadder {
         return 0;
     }
 
+    public int ladderLengthBidirectionalBFS(String beginWord, String endWord, List<String> wordList) {
+        // Convert the wordList to Set for constant time lookup
+        final Set<String> words = new HashSet<>(wordList);
+        // Check if endWord is present in the words set
+        if (!words.contains(endWord)) {
+            return 0;
+        }
+        // Perform Bidirectional BFS
+        final Set<String> beginSet = new HashSet<>();
+        beginSet.add(beginWord);
+        final Set<String> endSet = new HashSet<>();
+        endSet.add(endWord);
+        return bidirectionalBFS(beginSet, endSet, words, 1);
+    }
+
+    private int bidirectionalBFS(Set<String> beginSet, Set<String> endSet, Set<String> words, int level) {
+        // Edge case
+        if (beginSet.isEmpty() || endSet.isEmpty()) {
+            return 0;
+        }
+        // Always expand smaller set
+        if (beginSet.size() > endSet.size()) {
+            return bidirectionalBFS(endSet, beginSet, words, level);
+        }
+        // Set to store nodes for next level
+        final Set<String> nextLevel = new HashSet<>();
+        for (String word : beginSet) {
+            final char[] characters = word.toCharArray();
+            for (int i = 0; i < characters.length; i++) {
+                // Preserve current character
+                final char originalCharacter = characters[i];
+                // Try every character from 'a' to 'z' at current index
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if (c == originalCharacter) {
+                        continue;
+                    }
+                    characters[i] = c;
+                    final String tempWord = new String(characters);
+                    // If we have found the endWord
+                    if (endSet.contains(tempWord)) {
+                        return level + 1;
+                    }
+                    // If tempWord doesn't exist in the words
+                    if (words.contains(tempWord)) {
+                        nextLevel.add(tempWord);
+                        words.remove(tempWord);
+                    }
+                }
+                characters[i] = originalCharacter;
+            }
+        }
+        return bidirectionalBFS(nextLevel, endSet, words, level + 1);
+    }
+
     public static void main(String[] args) {
         final WordLadder wordLadder = new WordLadder();
 
@@ -66,10 +120,12 @@ public class WordLadder {
         String endWord = "cog";
         List<String> wordList = List.of("hot", "dot", "dog", "lot", "log", "cog");
         System.out.println(wordLadder.ladderLength(beginWord, endWord, wordList));
+        System.out.println(wordLadder.ladderLengthBidirectionalBFS(beginWord, endWord, wordList));
 
         beginWord = "hit";
         endWord = "cog";
         wordList = List.of("hot", "dot", "dog", "lot", "log");
         System.out.println(wordLadder.ladderLength(beginWord, endWord, wordList));
+        System.out.println(wordLadder.ladderLengthBidirectionalBFS(beginWord, endWord, wordList));
     }
 }
